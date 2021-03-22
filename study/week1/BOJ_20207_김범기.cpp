afbeb4e45213473f7d00fb3bca18488fcd515872
qjarl <qjarl592@gmail,com>
2021-03-22 21:58:02 +0900

    HW 20207 김범기

---
+++ study/week1/BOJ_20207_김범기.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int year[367];
+
+int main() {
+	int n;
+	int maxdate = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int sd, ed;
+		cin >> sd >> ed;
+		maxdate = max(maxdate, ed);
+		for (int j = sd; j <= ed; j++) {
+			year[j]++;
+		}
+	}
+	int ans = 0;
+	int maxs = 0;
+	int slength = 0;
+	for (int i = 1; i <= maxdate + 1; i++) {
+		if (year[i] == 0) {
+			ans += maxs * slength;
+			maxs = 0;
+			slength = 0;
+		}
+		else {
+			slength++;
+			maxs = max(maxs, year[i]);
+		}
+	}
+	cout << ans;
+}
