6e5cb1c72b37dba6e0275748ff8e44f13cf80e67
WooWan <woohobi@gmail.com>
2021-04-09 19:59:59 +0900

    HW 14728 우창완

---
+++ study/week2/team1/BOJ_14728_우창완.py
@@ -0,0 +1,16 @@
+import sys
+
+n, t= map(int,input().split())
+dp=[[0]*(t+1) for _ in range(n+1)]
+
+arr=[0]*(n+1)
+for i in range(1,n+1):
+    #time, score
+    arr[i]=(list(map(int ,sys.stdin.readline().split())))
+
+for i in range(1,n+1):
+    for j in range(1,t+1):
+        if j-arr[i][0]>=0:
+            dp[i][j]=max(dp[i-1][j], dp[i-1][j-arr[i][0]]+arr[i][1])
+        else: dp[i][j]=dp[i-1][j]
+print(dp[n][-1])
