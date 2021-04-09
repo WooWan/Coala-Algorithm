cc2a60d415a6f61423bb00430d7f4f4b2f86ac28
qjarl <qjarl592@gmail,com>
2021-04-09 18:48:32 +0900

    'BOJ_2624_김범기'

---
+++ study/week2/team1/BOJ_2624_김범기.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int dp[10001] = { 1, };
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int t, k;
+	cin >> t >> k;
+	for (int i = 1; i <= k; i++) {
+		int p, n;
+		cin >> p >> n;
+		for (int j = t; j >= 0; j--) {
+			for (int k = 1; k <= n; k++) {
+				int tp = p * k;
+				if (j - tp >= 0) dp[j] += dp[j - tp];
+			}
+		}
+	}
+	cout << dp[t];
+}
