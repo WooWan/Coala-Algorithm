cb0dfcea8a856397494f0f66c97b7c8916c527e3
dohi1004 <dohi100430@gmail.com>
2021-05-12 04:16:37 +0900

    HW 14226 이도희

---
+++ study/week4/team1/BOJ_14226_이도희.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+from collections import deque
+
+n = int(input())
+dist = [[-1]* (n+1) for _ in range(n+1)]
+q = deque()
+q.append((1,0))  # 화면 이모티콘 개수, 클립보드 이모티콘 개수
+dist[1][0] = 0
+while q:
+    s,c = q.popleft()
+    if dist[s][s] == -1: # 화면 이모티콘 클립보드에 모두 저장
+        dist[s][s] = dist[s][c] + 1
+        q.append((s,s)) 
+    if s+c <= n and dist[s+c][c] == -1: # 클립보드 -> 화면에 붙여넣기
+        dist[s+c][c] = dist[s][c] + 1
+        q.append((s+c, c))
+    if s-1 >= 0 and dist[s-1][c] == -1: # 이모티콘 삭제
+        dist[s-1][c] = dist[s][c] + 1
+        q.append((s-1, c))
+answer = -1
+for i in range(n+1):
+    if dist[n][i] != -1:
+        if answer == -1 or answer > dist[n][i]:
+            answer = dist[n][i]
+print(answer)
+
