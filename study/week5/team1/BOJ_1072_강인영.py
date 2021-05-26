52dc2a80373ba5083de1ecc6ff223850321adda5
KangInyeong <KangInyeong@github.com>
2021-05-26 12:17:46 +0900

    HW 1072 강인영

---
+++ study/week5/team1/BOJ_1072_강인영.py
@@ -0,0 +1,22 @@
+x, y = map(int, input().split())
+z = (y*100) // x
+
+if z >= 99:
+  print(-1)
+
+else:
+  answer = 0
+  left = 1
+  right = x
+
+  while left <= right:
+    mid = (left + right) // 2
+    if(y+mid) * 100 // (x+mid) <= z:
+      left = mid + 1
+    else:
+      answer = mid
+      right = mid -1
+
+print(answer)
+
+#https://hillier.tistory.com/70 참고했습니다.
